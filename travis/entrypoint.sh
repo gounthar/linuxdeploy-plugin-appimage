3b5a4f4d1941c9dab5f452236b55913a46cd9a2a
Bruno Verachten <gounthar@gmail.com>
2020-02-22 19:01:19 +0100

    Update entrypoint.sh

--- travis/entrypoint.sh
+++ travis/entrypoint.sh
@@ -4,6 +4,6 @@
 # before you ask: yes, the binaries will work on CentOS 6 even without devtoolset (they somehow partially link C++
 # things statically while using others from the system...)
 # so, basically, it's magic!
-source /opt/rh/devtoolset-*/enable
+#source /opt/rh/devtoolset-*/enable
 
 exec "$@"
